d79eca730d1e93af00d8edc54d67e9ef348c78ea
sai pranesh mukkala <saipranesh.mukkala@sjsu.edu>
2017-08-02 08:18:50 -0700

    Initial layout setup : includes coordinator layout, appbar layout, toolbar and tab, along with nested scroll view

--- app/src/main/java/com/myfitnesspal/assignment/newsapp/MainActivity.java
+++ app/src/main/java/com/myfitnesspal/assignment/newsapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.myfitnesspal.assignment.newsapp;
 
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)
+                findViewById(R.id.collapsing_toolbar_layout);
+
+        AppBarLayout appBarLayout =(AppBarLayout) findViewById(R.id.app_bar_layout);
+
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = false;
+            int scrollRange = -1;
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbarLayout.setTitle(" ");
+                    isShow = true;
+                } else if(isShow) {
+                    collapsingToolbarLayout.setTitle(getString(R.string.app_name));
+                    isShow = false;
+                }
+            }
+        });
     }
 }
